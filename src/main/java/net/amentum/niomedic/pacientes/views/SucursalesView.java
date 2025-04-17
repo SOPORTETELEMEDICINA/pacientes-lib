63d3a5b6659e25b2b5ace023b3b468f22922b7d2
Adrian Arias <sauladrian.arias@gmail.com>
2025-04-16 22:39:55 -0600

    Sucursales

---
+++ src/main/java/net/amentum/niomedic/pacientes/views/SucursalesView.java
@@ -0,0 +1,17 @@
+package net.amentum.niomedic.pacientes.views;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class SucursalesView {
+
+    Integer idSucursal;
+    String nombre;
+
+}
